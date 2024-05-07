e7a195b333a8e14cd9fc1f163a7de2f4a259a669
Ujjayraj <23092232@imail.sunway.edu.my>
2024-05-07 09:55:28 +0800

    jhgfxxdcfvgbhn

--- AeroCheck.java
+++ AeroCheck.java
@@ -1,115 +1,119 @@
-/*
-group assignment for Java OOM COURSEWORK
-
-1) UJJAYRAJ A/L B RAJ MANI RAO = 23092232
-2) CHEN WEI HANG = 23103427
-3) CHOO TING FENG=23064140 
-4) Aakash Pai = 23047350
-*/
-
-import java.time.LocalDate;
+/*import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class AeroCheck
-{
+public class AeroCheck {
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
-        int desiredOption;
+        boolean restart = true;
 
+        while (restart) {
+            int desiredOption;
             welcomeScreen(1);
-            System.out.println("\n\t\t\t\t\t+++++++++++++ Welcome to AeroCheck AirLines +++++++++++++\n\nTo Further Proceed, Please enter a value.");
+            System.out.println("\n\n\t\t\t\t\t+++++++++++++ Welcome to AeroCheck AirLines +++++++++++++\n\nTo Further Proceed, Please enter a value.");
             while (true) {
-            System.out.println("\n\n\t\t(0) Exit.");
-            System.out.println("\t\t(1) Single Passenger Check-in.");
-            System.out.println("\t\t(2) Group Passenger Check-in.");
-            System.out.print("Enter the desired option:    ");
-            try {
-                desiredOption = read.nextInt();
-            } catch (InputMismatchException e) {
-                System.out.println("\nInvalid input. Please enter a valid value.\n");
-                read.next(); // discard the invalid input
-                continue;
-            }
-
-            if (desiredOption < 0 || desiredOption > 2) {
-                System.out.print("\nERROR!! Please enter value between 0 - 2. Enter the value again.\n");
-                continue;
-            }
-
-            // Process the desired option
-            break;
-        }
-
-        do {
-            Scanner read1 = new Scanner(System.in);
-
-            if (desiredOption == 1) {
-                
-                System.out.print("\nEnter Full Name: ");
-                String name = read1.nextLine();
-                
-                System.out.print("Age: ");
-                int age = getValidInt(read1, "Invalid age. Please enter a valid age.");
-
-                System.out.print("Gender (M/F): ");
-                char gender = getValidChar(read1, "Invalid gender. Please enter M or F.");
-
-                System.out.print("Passport Number: ");
-                long passNum = getValidLong(read1, "Invalid Passport number. Please enter a valid Passport number.");
-
-                System.out.println("Date of Flight (dd/mm/yyyy): ");
-                LocalDate dateOfFlight = getValidDate(read1, "Invalid Date. Please enter valid Date.");
-
-                System.out.println("\nSingle passenger details are as follows:\nName:\t" + name + "\nAge:\t" + age + " years old." + "\nGender:\t" + gender + "\nDate of Flight:\t" + dateOfFlight);
-
-                System.out.println("\t\tDo you want your luggage(s) to be handled? (Y/N)");
-                char handleLuggage = read1.next().charAt(0);
-                while (handleLuggage == 'Y'  || handleLuggage == 'y') {
-                    handlingLuggage();
-                    break;
+                System.out.println("\n\t\t(0) Exit.");
+                System.out.println("\t\t(1) Single Passenger Check-in.");
+                System.out.println("\t\t(2) Group Passenger Check-in.\n\n");
+                System.out.print("Enter the desired option: ");
+                try {
+                    desiredOption = read.nextInt();
+                } catch (InputMismatchException e) {
+                    System.out.println("\nInvalid input. Please enter a valid value.\n");
+                    read.next(); // discard the invalid input
+                    continue;
                 }
-                welcomeScreen(2);
-                return;
-            }
-            
-            if (desiredOption == 2) {    
-            System.out.print("Number of Passengers: ");
-            int numberOfPassengers = getValidInt(read, "Invalid number of passengers. Please enter a valid number.");
-    
-                for (int i = 0; i < numberOfPassengers; i++) {
-                    System.out.print("Enter Full Name of Passenger " + (i + 1) + ": ");
-                    String name = read.next();
-        
-                    System.out.print("Age of Passenger " + (i + 1) + ": ");
-                    int age = getValidInt(read, "Invalid age. Please enter a valid age.");
-        
-                    System.out.print("Gender of Passenger " + (i + 1) + " (M/F): ");
-                    char gender = getValidChar(read, "Invalid gender. Please enter M or F.");
-        
-                    System.out.print("Passport Number of Passenger " + (i + 1) + ": ");
-                    long passportNumber = getValidLong(read, "Invalid passport number. Please enter a valid passport number.");
-        
-                    System.out.print("Date of Flight (dd/mm/yyyy) of Passenger " + (i + 1) + ": ");
-                    String dateOfFlight = read.next();
-        
-                    System.out.println("\nPassenger " + (i + 1) + " details are as follows:\nName:\t" + name + "\nAge:\t" + age + " years old." + "\nGender:\t" + gender + "\nDate of Flight(yyyy/dd/mm):\t" + dateOfFlight);
-                
-                    char handleLuggage = read1.next().charAt(0);
-                    while (handleLuggage == 'Y'  || handleLuggage == 'y') {
-                    handlingLuggage();
-                    break;
+
+                if (desiredOption < 0 || desiredOption > 2) {
+                    System.out.print("\nERROR!! Please enter value between 0 - 2. Enter the value again.\n");
+                    continue;
                 }
-                }
+
+                // Process the desired option
                 break;
             }
-        } 
-        while (desiredOption != 0);
 
+            if (!restart) {
+                // Exit the program
+                break;
+            }
+
+            if (desiredOption == 1) {
+                singlePassengerCheckIn(read);
+            } else if (desiredOption == 2) {
+                groupPassengerCheckIn(read);
+            }
+
+            if (desiredOption == 0) {
+                System.out.print("\nThank you for using our platform.\n");
+                System.out.print("Do you want to restart? (true/false): ");
+                restart = read.nextBoolean();
+                break;
+            }
+
+        }
+
+        System.out.println("Program ended.");
+    }
+
+    private static void singlePassengerCheckIn(Scanner read) {
+        System.out.print("\nEnter Full Name: ");
+        String name = read.nextLine();
+
+        System.out.print("\nEnter your Age: ");
+        int age = getValidInt(read, "Invalid age. Please enter a valid age.");
+
+        System.out.print("Your Gender (M/F) ?: ");
+        char gender = getValidChar(read, "Invalid gender. Please enter M or F.");
+
+        System.out.print("Passport Number: ");
+        long passNum = getValidLong(read, "Invalid Passport number. Please enter a valid Passport number.");
+
+        System.out.println("Date of Flight (dd/mm/yyyy): ");
+        LocalDate dateOfFlight = getValidDate(read, "Invalid Date. Please enter valid Date.");
+
+        System.out.println("\nSingle passenger details are as follows:\nName:\t" + name + "\nAge:\t" + age + " years old." + "\nGender:\t" + gender + "\nDate of Flight:\t" + dateOfFlight);
+
+        System.out.println("\t\tDo you want your luggage(s) to be handled? (Y/N)");
+        char handleLuggage = read.next().charAt(0);
+        while (handleLuggage == 'Y' || handleLuggage == 'y') {
+            handlingLuggage();
+            break;
+        }
         welcomeScreen(2);
-        return;
+    }
+
+    private static void groupPassengerCheckIn(Scanner read) {
+        System.out.print("Number of Passengers: ");
+        int numberOfPassengers = getValidInt(read, "Invalid number of passengers. Please enter a valid number.");
+
+        for (int i = 0; i < numberOfPassengers; i++) {
+            System.out.print("Enter Full Name of Passenger " + (i + 1) + ": ");
+            String name = read.next();
+
+            System.out.print("Age of Passenger " + (i + 1) + ": ");
+            int age = getValidInt(read, "Invalid age. Please enter a valid age.");
+
+            System.out.print("Gender of Passenger " + (i + 1) + " (M/F): ");
+            char gender = getValidChar(read, "Invalid gender. Please enter M or F.");
+
+            System.out.print("Passport Number of Passenger " + (i + 1) + ": ");
+            long passportNumber = getValidLong(read, "Invalid passport number. Please enter a valid passport number.");
+
+            System.out.print("Date of Flight (dd/mm/yyyy) of Passenger " + (i + 1) + ": ");
+            String dateOfFlight = read.next();
+
+            System.out.println("\nPassenger " + (i + 1) + " details are as follows:\nName:\t" + name + "\nAge:\t" + age + " years old." + "\nGender:\t" + gender + "\nDate of Flight(yyyy/dd/mm):\t" + dateOfFlight);
+
+            char handleLuggage = read.next().charAt(0);
+            while (handleLuggage == 'Y' || handleLuggage == 'y') {
+                handlingLuggage();
+                break;
+            }
+        }
+        welcomeScreen(2);
     }
 
     private static int getValidInt(Scanner read, String errorMessage) {
@@ -163,12 +167,12 @@ public class AeroCheck
         }
     }
 
-    private static void handlingLuggage(){
+    private static void handlingLuggage() {
         Scanner read1 = new Scanner(System.in);
         System.out.println("\t\tPlease place your luggage(s) on the weighing scale");
         System.out.println("\t\tPress any key to Continue.");
         read1.next();
-        System.out.println("\t\tProccessing Weight...");
+        System.out.println("\t\tProcessing Weight...");
         System.out.println("\t\tPrinting baggage tag...");
         System.out.println("\t\tCompleted.\n");
         System.out.println("\t\tPress any key to Continue.");
@@ -195,8 +199,7 @@ public class AeroCheck
                                                                                                                                                                                                                                                   
                                                                                                                                                                                 \s
                     """;
-        }
-        if (option == 2) {
+        } else if (option == 2) {
             artWork = """
 
  .d8888b.  888                        888            d8b                .d8888b.                                                       .d888          888      888 
@@ -212,11 +215,9 @@ Y88b  d88P 888  888 Y8b.     Y88b.    888 "88b       888 888  888      Y88b  d88
                                                                                                                                                                    
 
                     """;
-        System.out.println(artWork);
+            System.out.println(artWork);
         }
-
     }
 }
 
-
-
+*/
