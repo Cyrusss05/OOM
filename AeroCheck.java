9da0598a4bf4865a915e5c6e917a78449818734e
Cyrusss05 <145358052+Cyrusss05@users.noreply.github.com>
2024-05-09 09:13:47 +0800

    .

--- AeroCheck.java
+++ AeroCheck.java
@@ -204,6 +204,7 @@ public class AeroCheck {
 8888P   Y8888 Y8b.     888 Y88b.   Y88..88P 888  888  888 Y8b.         Y88b. Y88..88P      d8888888888 Y8b.     888    Y88..88P Y88b  d88P 888  888 Y8b.     Y88b.    888 "88b
 888P     Y888  "Y8888  888  "Y8888P "Y88P"  888  888  888  "Y8888       "Y888 "Y88P"      d88P     888  "Y8888  888     "Y88P"   "Y8888P"  888  888  "Y8888   "Y8888P 888  888
                             """;
+        System.out.println(artWork);
         }
         if (option == 2) {
             artWork ="""
@@ -221,8 +222,9 @@ Y88b  d88P 888  888 Y8b.     Y88b.    888 "88b       888 888  888      Y88b  d88
                                                                                                                                                                    
 
                     """;
+        System.out.println(artWork);
         }
-        else {
+        if (option == 3) {
             artWork = """
                 \n\n
 888    888          888              d8b                                    888    888                                                       888
@@ -237,7 +239,7 @@ Y88b  d88P 888  888 Y8b.     Y88b.    888 "88b       888 888  888      Y88b  d88
                         888                                                                                                     Y8b d88P        
                         888                                                                                                      "Y88P"         
             """;
-        }
         System.out.println(artWork);
+        }
     }
 }
