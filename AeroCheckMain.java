8c391c13a4ef1f5a2a2126df8fd8221c9cc2dfec
Ujjayraj <65146978+ZeuS-05@users.noreply.github.com>
2024-05-11 20:05:15 +0800

    change main file name

--- AeroCheckMain.java
+++ AeroCheckMain.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class AeroCheck {
+public class AeroCheckMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Artwork art = new Artwork();
