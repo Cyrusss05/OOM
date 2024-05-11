cdc81aa7e0b34148df74fd07933265d227cd58c5
Ujjayraj <65146978+ZeuS-05@users.noreply.github.com>
2024-05-11 20:54:17 +0800

    not applicable info for kg

--- Passenger.java
+++ Passenger.java
@@ -23,8 +23,10 @@ public class Passenger {
 
 
     public void displayPassengerDetails() {
-        // Check if specialNeedsAssistance is true or false and set assist accordingly
+        //Check if specialNeedsAssistance is true or false and set assist accordingly
         String assist = specialNeedsAssistance ? "Required" : "Not Required";
+        //check when luggage is applicable or not 
+        String luggageInfo = luggageWeight == 0.0 ? "Not Applicable" : luggageWeight + " Kg";
 
         System.out.println("___________________________________________________________________________________________________________________________________________________");
         System.out.println("| Passenger Details");
@@ -33,7 +35,7 @@ public class Passenger {
         System.out.println(  "| Gender                     : " + gender);
         System.out.println(  "| Passport Number            : " + passportNumber);
         System.out.println(  "| Date of Flight (yyyy/mm/dd): " + dateOfFlight);
-        System.out.println(  "| Luggage Weight             : " + luggageWeight + " Kg");
+        System.out.println(  "| Luggage Weight             : " + luggageInfo);
         System.out.println(  "| Special Needs Assistance   : " + assist);
         System.out.println(  "| Bag Tac number             : " + baggageTag);
         System.out.println("|__________________________________________________________________________________________________________________________________________________");
