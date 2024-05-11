9bc8609a8c513ed98babc8887590ac4ffebd7c8e
Ujjayraj <65146978+ZeuS-05@users.noreply.github.com>
2024-05-11 23:36:15 +0800

    push for Aakash

--- AeroCheckMain.java
+++ AeroCheckMain.java
@@ -6,6 +6,7 @@ public class AeroCheckMain {
         Artwork art = new Artwork();
         int desiredOption;
         art.welcomeScreen(1);
+        
         System.out.println("\n\t\t\t\t\t+++++++++++++ Welcome to AeroCheck AirLines +++++++++++++\n\nTo Further Proceed, Please enter a value.");
 
         desiredOption = MenuHandler.displayMenuAndGetOption(scanner);
