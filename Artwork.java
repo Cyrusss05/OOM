d89b4001d52fc0ffb6bd6d7eef34e99795e9ed2f
Ujjayraj <65146978+ZeuS-05@users.noreply.github.com>
2024-05-11 16:49:25 +0800

    spacings

--- Artwork.java
+++ Artwork.java
@@ -13,7 +13,7 @@ public class Artwork {
 8888P   Y8888 Y8b.     888 Y88b.   Y88..88P 888  888  888 Y8b.         Y88b. Y88..88P      d8888888888 Y8b.     888    Y88..88P Y88b  d88P 888  888 Y8b.     Y88b.    888 "88b
 888P     Y888  "Y8888  888  "Y8888P "Y88P"  888  888  888  "Y8888       "Y888 "Y88P"      d88P     888  "Y8888  888     "Y88P"   "Y8888P"  888  888  "Y8888   "Y8888P 888  888
                             """;
-        System.out.println(artWork);
+        System.out.println("\n\n"+artWork);
         }
         if (option == 2) {
             artWork ="""
@@ -31,7 +31,7 @@ Y88b  d88P 888  888 Y8b.     Y88b.    888 "88b       888 888  888      Y88b  d88
                                                                                                                                                                    
 
                     """;
-        System.out.println(artWork);
+        System.out.println("\n\n"+artWork);
         }
         if (option == 3) {
             artWork = """
@@ -48,7 +48,7 @@ Y88b  d88P 888  888 Y8b.     Y88b.    888 "88b       888 888  888      Y88b  d88
                         888                                                                                                     Y8b d88P        
                         888                                                                                                      "Y88P"         
             """;
-        System.out.println(artWork);
+        System.out.println("\n\n"+artWork);
         }
     }
 }
