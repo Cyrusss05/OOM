5cdcf429b65b3d6d4af746412fa2a9b2cdf50bfb
Ujjayraj <65146978+ZeuS-05@users.noreply.github.com>
2024-05-11 15:41:06 +0800

    made special assistance its own file

--- AeroCheck.java
+++ AeroCheck.java
@@ -116,7 +116,8 @@ public class AeroCheck{
                 char specialNeeds = scanner.next().charAt(0);
                 switch(Character.toUpperCase(specialNeeds)){
                     case 'Y': {
-                        specialNeedsAssistance();
+                        SpecialNeedsAssistant assistant = new SpecialNeedsAssistant();
+                        assistant.specialNeedsAssistance();
                         validInput = true;
                         break;
                     }
@@ -223,7 +224,8 @@ public class AeroCheck{
                 char specialNeeds = scanner.next().charAt(0);
                 switch(Character.toUpperCase(specialNeeds)){
                     case 'Y': {
-                        specialNeedsAssistance();
+                        SpecialNeedsAssistant assistant = new SpecialNeedsAssistant();
+                        assistant.specialNeedsAssistance();
                         validInput = true;
                         break;
                     }
@@ -242,41 +244,7 @@ public class AeroCheck{
         }
         art.welcomeScreen(2);
     }
-    private static void specialNeedsAssistance()
-    {
-        Artwork art = new Artwork();
-        //  Handle special needs
-        Scanner scanner = new Scanner(System.in);
-        int snOptions;
-        while (true) {
-            System.out.println("\n\n\t\t(0) Return to check-in.");
-            System.out.println("\t\t(1) Wheelchair Assistance.\n\n");
-            System.out.print("Enter the desired option:    ");
-            try {
-                snOptions = scanner.nextInt();
-                if (snOptions < 0 || snOptions > 1) {
-                    System.out.print("\nERROR!! Please enter value between 0 - 1. Enter the value again.\n");
-                    continue;
-                }
-                break;
-            } catch (InputMismatchException e) {
-                System.out.println("\nInvalid input. Please enter a valid value.\n");
-                scanner.nextLine(); // Consume the invalid input
-            }
-        }
-        switch (snOptions) {
-            case 1:
-                art.welcomeScreen(3);
-                System.out.println("Please wait patiently for our staff to locate to you.");
-                break;
-            case 0:
-                break;
-            default:
-                System.out.println("\nInvalid option. Please enter a valid option.\n");
-                break;
-        }
-        scanner.close();
-    }
+    
     private static LocalDate getValidDate(Scanner read, String errorMessage) {
         while (true) {
             String dateOfFlight = read.next();
