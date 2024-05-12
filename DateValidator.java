4a639639b8ff4e248c762252a20731a11ac0ec6e
Ujjayraj <65146978+ZeuS-05@users.noreply.github.com>
2024-05-12 18:25:28 +0800

    Update DateValidator.java

--- DateValidator.java
+++ DateValidator.java
@@ -6,13 +6,20 @@ import java.util.Scanner;
 public class DateValidator {
     public static LocalDate getValidDate(Scanner scanner, String errorMessage) {
         while (true) {
-            String dateOfFlight = scanner.next();
+            String dateInput = scanner.next();
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
             try {
-                return LocalDate.parse(dateOfFlight, formatter);
+                LocalDate enteredDate = LocalDate.parse(dateInput, formatter);
+                LocalDate currentDate = LocalDate.now();
+                if (enteredDate.isBefore(currentDate)) {
+                    System.out.println("Error: Date cannot be before the current date.");
+                    System.out.print("Enter a valid future date (dd/MM/yyyy): ");
+                } else {
+                    return enteredDate;
+                }
             } catch (DateTimeParseException e) {
                 System.out.println(errorMessage);
-                System.out.print("Enter a valid date format (dd/mm/yyyy): ");
+                System.out.print("Enter a valid date format (dd/MM/yyyy): ");
             }
         }
     }
